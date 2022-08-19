15764b08b43dbbc6cda7679b8521f04839385dc1
nusrattasnim <109256470+nusrattasnim@users.noreply.github.com>
2022-08-19 08:06:28 +0600

    Add files via upload

---
+++ Conditional Statements/odd_even.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int num;
+    printf("Enter a number\n");
+    scanf("%d",&num);
+
+    if(num%2==0)
+        printf("%d is even number",num);
+    else
+        printf("%d is odd number",num);
+}
