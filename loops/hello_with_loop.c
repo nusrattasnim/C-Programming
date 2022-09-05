4a6dcb03064c475665c45dbefb60b8f48950aecf
nusrattasnim <109256470+nusrattasnim@users.noreply.github.com>
2022-09-05 08:45:42 +0600

    Add files via upload

---
+++ loops/hello_with_loop.c
@@ -0,0 +1,12 @@
+//Write a program to print Hello World 10 times using a loop.
+
+#include<stdio.h>
+int main()
+{
+    int i=1;
+    while(i<=10)
+    {
+        printf("%d. hello world\n",i);
+        i++;
+    }
+}
